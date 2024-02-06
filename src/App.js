9076472d0e2e14f0c345f34030392e3d3d2079e5
shadrackbenjamin <131531398+shadrackbenjamin@users.noreply.github.com>
2024-02-06 16:43:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Routes, Route } from "react-router-dom";
+import  Container  from 'react-bootstrap/Container';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar expand= "lg" className='fixed-top bg-body-tertiary shadow'>
+      <Container>
+        <Navbar.Brand>
+          <Link to="/" className='navbar-brand text-success fw-semibold'>
+            Shady's Restaurant
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className='me-auto justify-content-end w-100'>
+            <Nav.Link href="/" className="active text-uppercase">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/menu" className=" text-uppercase">
+              Menu
+            </Nav.Link>
+            <Nav.Link href="/About" className=" text-uppercase">
+              About
+            </Nav.Link>
+            <Nav.Link href="/contact" className=" text-uppercase">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+
+    <footer className='bg-body-tertiary'>
+      <p className='p-3 m-0 text-center'> copyright @ made by 99Angels</p>
+    </footer>
+
     </div>
   );
 }
---
+++ src/components/MenuBtn.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+
+export function MenuBtn() {
+    return (
+        <Link to="/menu">
+        <button type="button" className="btn btn-success btn-lg">
+        Our Full Menu
+        </button>
+        </Link>
+    );
+}
